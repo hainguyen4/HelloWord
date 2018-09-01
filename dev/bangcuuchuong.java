933c4cf5825f76a0d118d31992889715a776d1ad
chiur0y0204@gmail.com <hainguyen4>
2018-09-01 12:13:15 +0700

    File Bang cuu chuong

---
+++ dev/bangcuuchuong.java
@@ -0,0 +1,11 @@
+package hainguyen.dev;
+ public class bangcuuchuong{
+ public static void main (String[]args) {
+	 for (int i =1;i <=9 ; i++) {
+		 for (int j=1;j <=10; j++) {
+			 System.out.print(j + "*" + i + "=" + (i*j) + "\t");
+		 }
+	 System.out.println("");
+	 }
+ }
+}
